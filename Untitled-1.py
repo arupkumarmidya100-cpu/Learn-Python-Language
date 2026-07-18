f25615a3c63d36c1507f80dd754467fe6b3e5715
Arup <arupkumarmidya100@gmail.com>
2026-07-18 21:58:07 +0530

    First push

---
+++ Untitled-1.py
@@ -0,0 +1,12 @@
+# # Name ="Arup"
+# # Age =20
+# # price = 23.980
+# # A = None
+# # print(type(Name))
+# # print(type(Age))
+# # print(type(price))
+# # print(type(A))  
+
+# first = int(input("Enter Number a:"))
+# second = int(input("Enter number b:"))
+# print("The sum is :", first+second)
